79809e71f6010079dd0ecb2783a7e67db110ee54
Sean Kelley <sean.kelley@neudesic.com>
2023-08-10 08:29:32 -0400

    fills out person.cs

--- MockAPI.Domain/Person.cs
+++ MockAPI.Domain/Person.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace MockAPI.Domain
 {
-    internal class Person
+    public class Person
     {
+        public int BusinessEntityId { get; set; }
+        public string PersonType { get; set; }
+        public int NameStyle { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public char MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string suffix { get; set; }
+        public int EmailPromotion { get; set; }
+        public XmlDocument AdditonalContactInfo { get; set; }
+        public XmlDocument Demogaphics { get; set; }
+        public string rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
     }
 }
