f31a642d3a42efb7dc5860c2b9df0a903961a22c
Sean Kelley <sean.kelley@neudesic.com>
2023-08-10 15:15:46 -0400

    Implements PeopleController

---
+++ MockAPI.Api/Controllers/PeopleController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MockAPI.Api.RequestResponseObjects;
+using MockAPI.Api.Services;
+using MockAPI.Data;
+using MockAPI.Domain;
+using SnackTrack.Data;
+
+namespace MockAPI.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        private readonly PeopleService people;
+        public PeopleController(MockAPIContext dbContext)
+        {
+            people = new PeopleService(dbContext);
+        }
+
+        [HttpGet]
+        public IActionResult GetPerson(int BusinessEntityId)
+        {
+            try
+            {
+                return Ok(people.GetPerson(BusinessEntityId));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
